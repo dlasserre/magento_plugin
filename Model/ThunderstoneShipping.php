936f804109d7934d3da8bbeb0e552a1e8678048f
Astaross <bacheville.dev@gmail.com>
2021-07-08 17:55:36 +0200

    Added custom thunderstone shipping

---
+++ Model/ThunderstoneShipping.php
@@ -0,0 +1,112 @@
+<?php
+
+
+namespace Thunderstone\Order\Model;
+
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\Method;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Psr\Log\LoggerInterface;
+
+class ThunderstoneShipping extends AbstractCarrier implements CarrierInterface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'thunderstone';
+
+    /**
+     * @var bool
+     */
+    protected $_isFixed = true;
+
+    /**
+     * @var ResultFactory
+     */
+    private $rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    private $rateMethodFactory;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ErrorFactory $rateErrorFactory
+     * @param LoggerInterface $logger
+     * @param ResultFactory $rateResultFactory
+     * @param MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory $rateErrorFactory,
+        LoggerInterface $logger,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+    }
+
+    /**
+     * Custom Shipping Rates Collector
+     *
+     * @param RateRequest $request
+     * @return Result|bool
+     */
+    public function collectRates(RateRequest $request)
+    {
+        /**
+         * Filter for enabling usage via module but not checkout
+         */
+        $input = file_get_contents('php://input');
+        if ($json = json_decode($input, true)) {
+            if(!isset($json['order'])){
+                return false;
+            }
+        }
+        else if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        /** @var Result $result */
+        $result = $this->rateResultFactory->create();
+
+        /** @var Method $method */
+        $method = $this->rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $shippingCost = (float)$this->getConfigData('shipping_cost');
+
+        $method->setPrice($shippingCost);
+        $method->setCost($shippingCost);
+
+        $result->append($method);
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
